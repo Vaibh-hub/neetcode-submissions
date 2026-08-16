bb1f1069eb92e477fa0282193c66550f8dedbdd0
Vaibh-hub <vaibhavm4285@gmail.com>
2026-08-16 17:36:00 +0530

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    private int maxProfit(int[] nums, int start, int end) {
+        int n = end - start + 1;
+        if (n == 1) {
+            return nums[start];
+        }
+        int[] dp = new int[n];
+        dp[0] = nums[start];
+
+        dp[1] = Math.max(nums[start], nums[start + 1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(
+                dp[i - 1],
+                dp[i - 2] + nums[start + i]
+            );
+        }
+
+        return dp[n - 1];
+    }
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        int case1 = maxProfit(nums, 0, n - 2);
+        int case2 = maxProfit(nums, 1, n - 1);
+        return Math.max(case1, case2);
+    }
+}
